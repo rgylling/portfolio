e7238562e021ff29cb3f7945630926866d07c9f3
Randy Gylling <MikeGylling@yahoo.com>
2016-04-12 19:27:11 -0700

    added some basic logic for my buttons

--- scripts/portfolio.js
+++ scripts/portfolio.js
@@ -2,19 +2,19 @@ var portData = [
    {
   title:'Project One',
   url:'www.hi.com',
-  snapshot:'../img/puppy1.jpg',
+  snapshot:'img/puppy1.jpg',
   body:'<p>This is where I talk about my project and stuff This is where I talk about my project and stuff This is where I talk about my project and stuff This is where I talk about my project and stuff This is where I talk about my project and stuff This is where I talk about my project and stuff This is where I talk about my project and stuff </p>'
 },
 {
   title:'Project Two',
   url:'www.who.com',
-  snapshot:'../img/puppy2.jpg',
+  snapshot:'img/puppy2.jpg',
   body:'</p>This is where I talk about my project and stuff This is where I talk about my project and stuff This is where I talk about my project and stuff This is where I talk about my project and stuff This is where I talk about my project and stuff This is where I talk about my project and stuff  This is where I talk about my project and stuff  </p>'
 },
 {
   title:'Project Three',
   url:'www.me.com',
-  snapshot:'../img/puppy3.jpg',
+  snapshot:'img/puppy3.jpg',
   body:'<p>This is where I talk about my project and stuff This is where I talk about my project and stuff This is where I talk about my project and stuff This is where I talk about my project and stuff This is where I talk about my project and stuff This is where I talk about my project and stuff This is where I talk about my project and stuff </p>'
 },
 ];
@@ -45,3 +45,19 @@ portData.forEach(function(ele) {
 articles.forEach(function(a){
   $('#articles').append(a.toHtml())
 });
+
+$('.hideme').hide();
+
+$('#second').on('click', function(){
+  $('.hideme').show();
+  $('.hideme2').hide();
+  $('html, body').animate({
+   scrollTop: $(".hideme").offset().top
+}, 1000);
+});
+
+$('#first').on('click', function () {
+  $("html, body").animate({ scrollTop: 0 }, 3000);
+  $('.hideme2').show();
+  $('.hideme').hide();
+})
