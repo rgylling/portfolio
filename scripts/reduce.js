1b82c3538dba5164144eee5c86f58056ce948570
Randy Gylling <MikeGylling@yahoo.com>
2016-04-19 21:21:24 -0700

    cleaned up my code

--- scripts/reduce.js
+++ scripts/reduce.js
@@ -1,24 +1,21 @@
-function wordCount(numbs) {
-  return numbs.split(' ').length;
-
-};
-
+//Counts the words in my body paragraphs
 Article.numWordsAll = function() {
   console.log(Article.all);
   console.log('doesthiswork????????');
   return Article.all.map(function(article) {
-    return wordCount(article.body); // Grab the words from the `article` `body`.
+    return article.body.split(' ').length;
     console.log('doesthisworknow');
   })
 .reduce(function(a, b) {
   console.log(a+b + 'fduhdfghufd');
-  return a+b;// Sum up all the values!
+  return a+b;
 });
 };
 
+//Appends the number of words in my body paragraphs
 function timerOn () {
-$( document ).ready(function() {
-    $('#reduce').text(Article.numWordsAll() + ' words in my articles');
-});
+  $( document ).ready(function() {
+    $('#reduce').text('fun fact, there are ' + Article.numWordsAll() + ' words in my articles!');
+  });
 };
-window.setTimeout(timerOn, 500);
+window.setTimeout(timerOn, 10);
