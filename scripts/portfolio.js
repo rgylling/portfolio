252c410fb66a5900bb7e40b8be98d748e0472588
rgylling <mikegylling@yahoo.com>
2016-04-21 12:48:03 -0700

    integrated first high order function

--- scripts/portfolio.js
+++ scripts/portfolio.js
@@ -42,7 +42,28 @@
 
 
 //Hides about section when user comes to the page
-  $('section .aboutsection').css('display','none');
+  //$('section .aboutsection').css('display','none');
+
+
+  function useNext(next) {
+    var aboutSection = $('section .aboutsection');
+
+    next(aboutSection);
+
+    console.log('everything loaded');
+  }
+
+  function hideAbout(s){ s.css('display','none'); };
+
+
+  useNext(hideAbout);
+
+
+
+
+
+
+
 
 //Shows about section and hides portfolio section
   $('nav li:nth-child(2)').click(function() {
