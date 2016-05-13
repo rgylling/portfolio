32009ca88e20031fcee354668ab62ee2f5fa175c
rgylling <mikegylling@yahoo.com>
2016-05-12 18:39:53 -0700

    re making portfolio

--- scripts/repoView.js
+++ scripts/repoView.js
@@ -1,16 +1,1 @@
-(function(module) {
-  var repoView = {};
-  var ui = function() {
-    var $about = $('#repoSection');
-    $about.show();
-  };
-  var repoTemplate = $('#repo-template').html();
-  var render = Handlebars.compile(repoTemplate);
-  repoView.index = function() {
-    ui();
-    $('#repoSection').append(
-      repos.with('name').map(render)
-    );
-  };
-  module.repoView = repoView;
-})(window);
+
