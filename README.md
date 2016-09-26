da363ebee6c3a0a4c8b32b331969ef81c4fb0502
rgylling <mikegylling@yahoo.com>
2016-09-25 17:26:22 -0700

    main changes done for updated portfolio

--- README.md
+++ README.md
@@ -1,15 +1,1 @@
-#Portfolio
-This contains all of my work for my portfolio labs
-#User Stories
-As a developer, I want my site to use valid and semantic markup, so that employers will love me.
-As the creator, I want the page to link to my social and GitHub pages, so that visitors can follow me, and I can build my audience.
-As a developer, I want portfolio items displayed with a repeatable template, so that I can reuse it, and abstract out the details for individual projects.
-#Strech Goals
-As a visitor, I want the site to look reasonable, so that I can read it on any device.
-As a visitor, I want the portfolio to show the newest projects on top so that I can easily see the developers recent work.
-As a visitor, I want relative timestamps on projects to give me a idea of how many days ago something was created.
 
-#Class-09 Functional Programming Documentation
-My first high order function will be a very simple test of my knowledge of high order functions. In my code you can see one line that is hiding my about me section as soon as the user loads the page.. I am going to make a function that hides whatever I pass it. Then I will make a function that accepts that function and also retrieves the element from the DOM utilizing Jquery.
-
-For my second high order function that generates a closure I will be taking input from a user in the form of their name. I will store their name in a variable. In my high order function I will accept a string as an argument based on which page the user is on eg: 'welcome to my homepage'. After I call the high order function with the string it will return a function with the users input (name) and append everything to the page appending something like hello (users name) welcome to my home page!
--- scripts/portfolio.js
+++ scripts/portfolio.js
@@ -64,3 +64,100 @@ $(document).ready(function (){
         }, 1000);
     });
 });
+
+$.fn.is_on_screen = function(){
+    var win = $(window);
+    var viewport = {
+        top : win.scrollTop(),
+        left : win.scrollLeft()
+    };
+    viewport.right = viewport.left + win.width();
+    viewport.bottom = viewport.top + win.height();
+
+    var bounds = this.offset();
+    bounds.right = bounds.left + this.outerWidth();
+    bounds.bottom = bounds.top + this.outerHeight();
+
+    return (!(viewport.right < bounds.left || viewport.left > bounds.right || viewport.bottom < bounds.top || viewport.top > bounds.bottom));
+};
+
+if( $('#charts').length > 0 ) { // if target element exists in DOM
+	if( $('#charts').is_on_screen() ) { // if target element is visible on screen after DOM loaded
+
+			 $('.html_chart').addClass('html');
+			 $('.css_chart').addClass('css');
+			 $('.js_chart').addClass('js');
+			 $('.wordpress_chart').addClass('wordpress');
+			 $('.ruby_chart').addClass('ruby');
+			 $('.ps_chart').addClass('ps');
+			 $('.git_chart').addClass('git');
+			 $('.bootstrap_chart').addClass('bootstrap');
+       $('.responsive_chart').addClass('responsive');
+	} else {
+			 $('.html_chart').removeClass('html');
+			 $('.css_chart').removeClass('css');
+			 $('.js_chart').removeClass('js');
+			 $('.wordpress_chart').removeClass('wordpress');
+			 $('.ruby_chart').removeClass('ruby');
+			 $('.ps_chart').removeClass('ps');
+			 $('.git_chart').removeClass('git');
+			 $('.bootstrap_chart').removeClass('bootstrap');
+       $('.responsive_chart').removeClass('responsive');
+	}
+}
+$(window).scroll(function(){ // bind window scroll event
+	if( $('#charts').length > 0 ) { // if target element exists in DOM
+		if( $('#charts').is_on_screen() ) { // if target element is visible on screen after DOM loaded
+
+			 $('.html_chart').addClass('html');
+			 $('.css_chart').addClass('css');
+			 $('.js_chart').addClass('js');
+			 $('.wordpress_chart').addClass('wordpress');
+			 $('.ruby_chart').addClass('ruby');
+			 $('.ps_chart').addClass('ps');
+			 $('.git_chart').addClass('git');
+			 $('.bootstrap_chart').addClass('bootstrap');
+       $('.responsive_chart').addClass('responsive');
+
+		} else {
+
+			 $('.html_chart').removeClass('html');
+			 $('.css_chart').removeClass('css');
+			 $('.js_chart').removeClass('js');
+			 $('.wordpress_chart').removeClass('wordpress');
+			 $('.ruby_chart').removeClass('ruby');
+			 $('.ps_chart').removeClass('ps');
+			 $('.git_chart').removeClass('git');
+			 $('.bootstrap_chart').removeClass('bootstrap');
+       $('.responsive_chart').removeClass('responsive');
+
+		}
+	}
+});
+
+var $animation_elements = $('.animation-element');
+var $window = $(window);
+
+function check_if_in_view() {
+  var window_height = $window.height();
+  var window_top_position = $window.scrollTop();
+  var window_bottom_position = (window_top_position + window_height);
+
+  $.each($animation_elements, function() {
+    var $element = $(this);
+    var element_height = $element.outerHeight();
+    var element_top_position = $element.offset().top;
+    var element_bottom_position = (element_top_position + element_height);
+
+    //check to see if this current container is within viewport
+    if ((element_bottom_position >= window_top_position) &&
+        (element_top_position <= window_bottom_position)) {
+      $element.addClass('in-view');
+    } else {
+      // $element.removeClass('in-view');
+    }
+  });
+}
+
+$window.on('scroll resize', check_if_in_view);
+$window.trigger('scroll');
